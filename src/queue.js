d3737416b35eb97bc3b066e8a7e5bb0a53a34e55
rydvone <rydvone@gmail.com>
2022-05-01 22:16:16 +0300

    feat: queue is done

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,23 +13,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
+ class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    let nodeLastValue = 0;
+    if(!(this.length)) {
+      return 1;
+    } else {
+      function recursive(link) {
+        let linkCurrentInner = link;
+        if(linkCurrentInner.next) {
+          recursive(linkCurrentInner.next);
+        }
+        return linkCurrentInner;
+      }
+      nodeLastValue = recursive(this.head);
+
+
+      return nodeLastValue;
+    }
+    
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    // throw new NotImplementedError('Not implemented');
+
+    const nodeQueue = new ListNode(value);
+    
+    if(this.length === 0) {
+      this.head = nodeQueue;
+    }
+    if(this.length) {
+      let linkCurrent = this.head;
+      while(linkCurrent.next) {
+        console.log(linkCurrent.value);
+        linkCurrent = linkCurrent.next;
+
+      }
+      linkCurrent.next = nodeQueue;
+    }
+    this.length++;
+    return this;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+
+    if(this.length === 0) {
+      return 1;
+    } else {
+      let linkHeadTemp = this.head.next;
+      const nodeFirstValue = this.head.value;
+      this.head = linkHeadTemp;
+
+      this.length--;
+      return nodeFirstValue;
+    }
   }
 }
+
 
 module.exports = {
   Queue
